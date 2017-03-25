90812bb2f6fde1813bb320b1c4bbdb22c88ee44b
Danish Suri <dsuri@ncsu.edu>
2017-03-24 23:52:23 -0400

    Added AsynTask to load data and parse json

--- app/src/main/java/com/example/android/quakereport/Adapters/ListDataAdapter.java
+++ app/src/main/java/com/example/android/quakereport/Adapters/ListDataAdapter.java
@@ -1,9 +1,12 @@
 package com.example.android.quakereport.Adapters;
 
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.android.quakereport.R;
@@ -38,6 +41,46 @@ public class ListDataAdapter extends BaseAdapter
         return i*8;
     }
 
+    private int getMagnitudeColor(double magnitude, ViewGroup viewGroup)
+    {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(viewGroup.getContext(), magnitudeColorResourceId);
+    }
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
@@ -46,10 +89,23 @@ public class ListDataAdapter extends BaseAdapter
         TextView loc = (TextView) view.findViewById(R.id.loc);
         TextView date = (TextView) view.findViewById(R.id.date);
         TextView time = (TextView) view.findViewById(R.id.time);
+        LinearLayout back = (LinearLayout)view.findViewById(R.id.back);
         mag.setText(data.get(i).getMagnitude());
         loc.setText(data.get(i).getLocation());
         date.setText(data.get(i).getDate());
         time.setText(data.get(i).getTime());
+        GradientDrawable magnitudeCircle = (GradientDrawable) back.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        try {
+            int magnitudeColor = getMagnitudeColor(Double.parseDouble(data.get(i).getMagnitude()),viewGroup);
+            magnitudeCircle.setColor(magnitudeColor);
+        }
+        catch (Exception e)
+        {
+
+        }
+
         return view;
     }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.quakereport;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,19 +26,23 @@ import com.example.android.quakereport.Adapters.ListDataAdapter;
 import com.example.android.quakereport.util.EarthquakeData;
 import com.example.android.quakereport.util.QueryUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
+    public static ListView earthquakeListView;
+    public static final String urlString = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<EarthquakeData> earthquakes = new ArrayList<>();
+
         /*earthquakes.add(new EarthquakeData("4.5","San Francisco","Feb, 20 2017"));
         earthquakes.add(new EarthquakeData("7.0","London","Feb, 20 2017"));
         earthquakes.add(new EarthquakeData("6.7","Tokyo","Feb, 20 2017"));
@@ -45,15 +51,49 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakes.add(new EarthquakeData("2.5","Rio de Janeiro","Feb, 20 2017"));
         earthquakes.add(new EarthquakeData("3.7","Paris","Feb, 20 2017"));*/
 
-        earthquakes = QueryUtils.extractEarthquakes();
-        System.out.println("LOG::Dann"+earthquakes.size());
+
 
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+         earthquakeListView= (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(new ListDataAdapter(earthquakes));
+        EarthquakeAsyncTask getData = new EarthquakeAsyncTask();
+        getData.execute(urlString);
+
+    }
+
+    public static void updateUI(ArrayList<EarthquakeData> data)
+    {
+        Log.d(LOG_TAG,"Update UI");
+        earthquakeListView.setAdapter(new ListDataAdapter(data));
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String,Void,List<EarthquakeData>>
+    {
+
+        @Override
+        protected List<EarthquakeData> doInBackground(String... strings) {
+            ArrayList<EarthquakeData> earthquakes=null;
+            try {
+                if(strings.length<1 || strings[0]==null)
+                    return null;
+                String response = QueryUtils.makeHttpRequest(strings[0]);
+                earthquakes = QueryUtils.extractEarthquakes(response);
+            }
+            catch (IOException e)
+            {
+             e.printStackTrace();
+            }
+            return earthquakes;
+        }
+
+        @Override
+        protected void onPostExecute(List<EarthquakeData> earthquakes) {
+         if(earthquakes==null)
+             return;
+            updateUI((ArrayList<EarthquakeData>) earthquakes);
+        }
     }
 }
